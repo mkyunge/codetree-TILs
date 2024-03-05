7ca1ab9da62c15c821b6f50501cb72917dc2ac53
mkyunge <97338443+mkyunge@users.noreply.github.com>
2024-03-06 00:33:31 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/합과 평균의 차/sub-of-average-and-sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    int c;
+
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d\n%d\n%d", a + b + c, (a + b + c) / 3, ((a + b + c) - ((a + b + c) / 3)));
+    return 0;
+}
