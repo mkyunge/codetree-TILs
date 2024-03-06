14bb4ea8762ece5d1a0e3bbcfb58b501ace553f2
mkyunge <97338443+mkyunge@users.noreply.github.com>
2024-03-06 21:21:47 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240306/살 수 있는 물건/things-able-to-buy.c
+++ 240306/살 수 있는 물건/things-able-to-buy.c
@@ -5,13 +5,13 @@ int main() {
 
     scanf("%d", &n);
 
-    if (n <= 3000)
-    {
-        printf("book");
-    }
-    else if (n <= 1000)
+    if (n <= 1000)
     {
         printf("mask");
+    }
+    else if (n <= 3000)
+    {
+        printf("book");
     }
     else
     {
