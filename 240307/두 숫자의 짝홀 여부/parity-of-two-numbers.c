7f421ca57e0f541732fccfcf668800a514fbc0cf
mkyunge <97338443+mkyunge@users.noreply.github.com>
2024-03-07 00:06:56 +0900

    Title: 두 숫자의 짝홀 여부, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240307/두 숫자의 짝홀 여부/parity-of-two-numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+
+    scanf("%d %d", &a, &b);
+
+    if (a % 2 == 0)
+    {
+        printf("even\n");
+    }
+    else
+    {
+        printf("odd\n");
+    }
+    if (b % 2 == 0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    return 0;
+}
