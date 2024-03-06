42def35d7c9baf5dce339ba0cea70a6c76135cd6
mkyunge <97338443+mkyunge@users.noreply.github.com>
2024-03-06 22:40:46 +0900

    Title: 최대 2번의 연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240306/최대 2번의 연산/up-to-2-calculations.c
+++ 240306/최대 2번의 연산/up-to-2-calculations.c
@@ -7,7 +7,11 @@ int main() {
 
     if (a % 2 == 0)
     {
-        printf("%d", (a / 2 + 1) / 2);
+        printf("%d", a / 2);
+        if (a % 2 == 1)
+        {
+            printf("%d", (a + 1) / 2);
+        }
     }
     if (a % 2 == 1)
     {
