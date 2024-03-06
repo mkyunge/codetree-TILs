3900da1d28f5fab73ce9f951a41c9bdd81660712
mkyunge <97338443+mkyunge@users.noreply.github.com>
2024-03-06 21:16:44 +0900

    Title: 물의 상태, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240306/물의 상태/state-of-water.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+
+    if(n < 0)
+    {
+        printf("ice");
+    }
+    else if(n >= 100)
+    {
+        printf("vapor");
+    }
+    else
+    {
+        "water";
+    }
+    return 0;
+}
