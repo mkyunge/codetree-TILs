7ffb10f44164f813f485627f94341b44052b9a65
mkyunge <97338443+mkyunge@users.noreply.github.com>
2024-03-07 22:46:28 +0900

    Title: 장학금, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240307/장학금/scholarship.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+
+    scanf("%d %d", &a, &b);
+
+    if (a >= 90 && b >= 95)
+        printf("100000");
+    else if (a >= 90 && b >= 90)
+        printf("50000");
+    else
+        printf("0");
+    return 0;
+}
