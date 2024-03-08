39f1e2ea549283d94ffb44fe61c74f150dae2489
mkyunge <97338443+mkyunge@users.noreply.github.com>
2024-03-09 01:10:42 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240309/별 n개 출력하기/print-n-stars.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+    int n;
+
+    scanf("%d", &n);
+
+    while (i <= 5)
+    {
+        printf("*\n", i);
+        i++;
+    }
+    return 0;
+}
