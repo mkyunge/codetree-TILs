fca258609508463a77d4473979b37876dc4148ab
mkyunge <97338443+mkyunge@users.noreply.github.com>
2024-03-08 15:15:44 +0900

    Title: 남녀노소 구분짓기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240308/남녀노소 구분짓기/sex-and-age.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+
+    scanf("%d %d", &a, &b);
+
+    if (a == 0)
+    {
+        if (b >= 19)
+        {
+            printf("MAN");
+        }
+        else
+        {
+            printf("BOY");
+        }
+    }
+    else
+    {
+        if (b >= 19)
+        {
+            printf("WOMAN");
+        }
+        else
+        {
+            printf("GIRL");
+        }
+    }
+    return 0;
+}
