61f01832f8bf18f3ea0b6ec493787c4047c3c855
mkyunge <97338443+mkyunge@users.noreply.github.com>
2024-03-08 20:30:54 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240308/코로나 메뉴얼/covid-manual.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    char a_c;
+    int a_t;
+    char b_c;
+    int b_t;
+    char c_c;
+    int c_t;
+
+    scanf("%c %d %c %d %c %d", &a_c, &a_t, &b_c, &b_t, &c_c, &c_t);
+
+    if (a_c == 'Y' && a_t >= 37)
+    {
+        if (b_c == 'Y' && b_t >= 37 || c_c == 'Y' && c_t >= 37)
+        {
+            printf("E");
+        }
+        else
+        {
+            printf("N");
+        }
+    }
+    else if (b_c == 'Y' && b_t >= 37)
+    {
+        if (a_c == 'Y' && a_t >= 37 || c_c == 'Y' && c_t >= 37)
+        {
+            printf("E");
+        }
+        else
+        {
+            printf("N");
+        }
+    }
+    else if (c_c == 'Y' && c_t >= 37)
+    {
+        if (a_c == 'Y' && a_t >= 37 || b_c == 'Y' && b_t >= 37)
+        {
+            printf("E");
+        }
+        else
+        {
+            printf("N");
+        }
+    }
+    return 0;
+}
