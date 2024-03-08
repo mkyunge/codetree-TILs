9de0e8c96206a37906b45be70e29a208c8630b7e
mkyunge <97338443+mkyunge@users.noreply.github.com>
2024-03-09 01:03:12 +0900

    Title: 홀수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240309/홀수만 출력 2/output-only-odd-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    int i;
+
+    scanf("%d %d", &a, &b);
+
+    for (i = a; i >= b; i -= 2)
+    {
+        printf("%d ", i);
+    }
+    return 0;
+}
