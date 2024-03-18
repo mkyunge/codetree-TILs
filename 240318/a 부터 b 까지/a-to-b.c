276b37376c74b01fb2809d96940282402280347b
mkyunge <97338443+mkyunge@users.noreply.github.com>
2024-03-18 19:17:01 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240318/a 부터 b 까지/a-to-b.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("%d ", a);
+
+    for (int i = a; i < b; i)
+    {
+        if (i % 2 == 0)
+        {
+            i += 3;
+        }
+        else
+        {
+            i *= 2;
+        }
+        printf("%d ", i);
+    }
+    return 0;
+}
