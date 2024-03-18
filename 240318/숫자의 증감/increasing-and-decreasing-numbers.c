3f18783c3530954d0e620fe85bbdf2bc4bbbd04a
mkyunge <97338443+mkyunge@users.noreply.github.com>
2024-03-18 16:57:03 +0900

    Title: 숫자의 증감, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240318/숫자의 증감/increasing-and-decreasing-numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char c;
+    int n;
+    
+    scanf("%c %d", &c, &n);
+
+    if (c == 'A')
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            printf("%d ", i);
+        }
+    }
+    else if (c == 'D')
+    {
+        for (int i = n; i >= 1; i--)
+        {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
