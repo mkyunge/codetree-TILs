30cd5e1b4e9e9e3539d44c456fbd4b9aa318de0e
mkyunge <97338443+mkyunge@users.noreply.github.com>
2024-03-18 17:08:16 +0900

    Title: 2 또는 3의 배수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240318/2 또는 3의 배수/multiple-of-2-or-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0 || i % 3 == 0)
+        {
+            printf("%d ", 1);
+        }
+        else
+        {
+            printf("%d ", 0);
+        }
+    }
+    return 0;
+}
