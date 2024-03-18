8cf80ede232248a2ba03378b92ea1295ac212f49
mkyunge <97338443+mkyunge@users.noreply.github.com>
2024-03-18 16:59:37 +0900

    Title: 높은 수에서 낮은 수까지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240318/높은 수에서 낮은 수까지/from-high-to-low.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+
+    scanf("%d %d", &a, &b);
+
+    if (a > b)
+    {
+        for (int i = a; i >= b; i--)
+        {
+            printf("%d ", i);
+        }
+    }
+    else if (b > a)
+    {
+        for (int i = b; i >= a; i--)
+        {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
